898ed8add25ac3b369418df8a8642c915f7dff98
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2022-04-22 20:39:27 -0400

    feat: :sparkles: added initial content for landing page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<section class="text-center">
+  <header>
+    <h1 class="font-fell text-6xl font-semibold">
+      Welcome to the Creatures of DND
+    </h1>
+  </header>
+  <section class="pt-12 font-vollkorn">
+    <p class="text-xl">
+      Having difficulties choosing creatures to polymorph into? Or perhaps, ones
+      to roam the lands of your world?
+    </p>
+    <p class="py-6 text-xl italic">Then let the dice decide for you.</p>
+    <button
+      type="button"
+      class="rounded-md bg-slate-700 py-1 px-4 text-xl text-slate-100"
+      >Roll</button
+    >
+  </section>
+</section>
+
+<!-- Houses the content -->
+<section>
+  <p>Where the monsters reside.</p>
+</section>
