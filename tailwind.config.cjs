fad3352d3d3182007b99f2aa6130a9070565b208
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2022-04-22 20:31:24 -0400

    style: :lipstick: added IM Fell and Vollkorn fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        fell: ['IM Fell English SC', 'serif'],
+        vollkorn: ['Vollkorn', 'serif'],
+      }
+    },
   },
   plugins: [],
 }
