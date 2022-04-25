30a2e2290132a7a81b2d06685a875c7d2751c4c2
Nathan Truong <88857875+nktruong@users.noreply.github.com>
2022-04-25 15:40:12 -0400

    revert: :fire: removed Monsters file as it was not needed

--- src/lib/components/Monsters.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  // -- Randomizer route --
-  // 1. user presses randomizer button
-  // 2. the program chooses a random # based on the monster count provided by API
-  // 3. then again through the API, chooses a random result (monster)
-  // 4. with the monster result, accesses the info about that creature
-  // 5. display monster and info on the card
-</script>
